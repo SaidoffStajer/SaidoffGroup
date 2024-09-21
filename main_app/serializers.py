aa72bbf52b66b6cebe445cb70f7e6fda18d88e32
shohrux <mahkamovshohrux99@gmail.com>
2024-09-21 17:15:02 +0500

    Serializers

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -4,25 +4,25 @@ from .models import Faq_category, Faq, Feedback, WhyUs,Partners, Team,Subscribe,
 class Faq_categorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Faq_category
-        fields = ('id', 'title')
+        fields = ('id', 'title_uz', 'title_en', 'title_ru',)
 
 
 class FaqSerializer(serializers.ModelSerializer):
     class Meta:
         model = Faq
-        fields = ('id', 'questions', 'answers', 'faq_page', 'faq')
+        fields = ('id', 'questions','questions_en', 'questions_ru', 'answers','answers_en','answers_ru', 'faq_page', 'faq')
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
-        fields = ('id', 'name', 'comment', 'profession', 'image')
+        fields = ('id', 'name','name_en', 'name_ru', 'comment','comment_en', 'comment_ru' ,'profession', 'image')
 
 
 class WhyUsSerializer(serializers.ModelSerializer):
     class Meta:
         model = WhyUs
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'title','title_en', 'title_ru', 'description', 'description_en', 'description_ru' )
 
 
 class PartnersSerializer(serializers.ModelSerializer):
@@ -34,17 +34,17 @@ class PartnersSerializer(serializers.ModelSerializer):
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields = ('id', 'name', 'image', 'profession', 'created_at')
+        fields = ('id', 'name','name_en', 'name_ru', 'image', 'profession','profession_en','profession_ru', 'created_at')
 
 
 class SubscribeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscribe
-        fields = ('id', 'full_name', 'phone_number', 'is_checked', 'created_at')
+        fields = ('id', 'full_name','full_name_en','full_name_ru', 'phone_number', 'is_checked', 'created_at')
     
 
 class CertificateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Certificate
-        fields = ('id', 'title', 'description', 'image')
+        fields = ('id', 'title','title_en','title_ru', 'description','description_en','description_ru', 'image')
 
